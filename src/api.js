0e4c50e3273844545fbe7ca22627c24b8b1f9993
root <root@LAPTOP-FH9SK6S7.localdomain>
2021-07-03 17:15:55 -0400

    adding offline cache

--- src/api.js
+++ src/api.js
@@ -64,11 +64,17 @@ const getToken = async (code) => {
 
 export const getEvents = async () => {
   NProgress.start();
-
+  
   if (window.location.href.startsWith("http://localhost")) {
-    NProgress.done();
-    return mockData;
-  }
+      NProgress.done();
+      return mockData;
+    }
+
+    if (!navigator.onLine) {
+      const data = localStorage.getItem("lastEvents");
+      NProgress.done();
+      return data?JSON.parse(data).events:[];;
+    }
 
   const token = await getAccessToken();
 
